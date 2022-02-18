6c554e9811ca005b7fda4c70cc97321f2229b6b5
Tomoki Yamagata <ymgtech@millenary.net>
2022-02-19 03:25:40 +0900

    [add] 設定の保存ストレージクラスを実装

---
+++ src/main/kotlin/net/millenary/intellij/plugin/onelogin_aws/setting/OneloginAwsAssumeRoleSettingsStorage.kt
@@ -0,0 +1,37 @@
+package net.millenary.intellij.plugin.onelogin_aws.setting
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+@State(name = "OneloginAwsAssumeRoleSettings", storages = [Storage("onelogin-aws-assume-role-settings.xml")])
+class OneloginAwsAssumeRoleSettingsStorage(
+
+  /**
+   * プラグインが有効か
+   */
+  var isEnabled: Boolean,
+
+  /**
+   * OneLogin SDK のクライアント ID
+   */
+  var oneloginSdkClientId: String,
+
+  /**
+   * OneLogin SDK のクライアントシークレット
+   */
+  var oneloginSdkClientSecret: String,
+
+  /**
+   * OneLogin で MFA 設定をしている場合に MFA をスキップできる送信元 IP アドレス
+   */
+  var oneloginSdkIp: String,
+) : PersistentStateComponent<OneloginAwsAssumeRoleSettingsStorage> {
+
+  override fun getState(): OneloginAwsAssumeRoleSettingsStorage = this
+
+  override fun loadState(state: OneloginAwsAssumeRoleSettingsStorage) {
+    XmlSerializerUtil.copyBean(state, this)
+  }
+}
