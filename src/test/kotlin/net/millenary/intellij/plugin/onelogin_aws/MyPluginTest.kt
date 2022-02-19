9717600a63f3846fd5f0c4d5fc61edc307c119a1
Tomoki Yamagata <ymgtech@millenary.net>
2022-02-19 23:51:43 +0900

    [remove] デフォルトのファイル群を削除

--- src/test/kotlin/net/millenary/intellij/plugin/onelogin_aws/MyPluginTest.kt
+++ src/test/kotlin/net/millenary/intellij/plugin/onelogin_aws/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package com.github.agataymgt.intellijoneloginawsassumeroleplugin
+package net.millenary.intellij.plugin.onelogin_aws
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
